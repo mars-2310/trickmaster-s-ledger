ab5653648bfeb72c92ec566fc68d0978b7d36c3d
mars-2310 <ms9066113@gmail.com>
2025-09-29 21:09:58 +0530

    init

---
+++ app/api/auth/route.ts
@@ -0,0 +1,53 @@
+import { prismaClient } from "@/app/lib/db";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers : [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET ?? "next-secret",
+  callbacks: {
+    async signIn(params) {
+
+      if (!params.user.email) {
+        return false;
+      }
+
+      try {
+           const existingUser = await prismaClient.user.findUnique({
+        where: { email: params.user.email },
+      });
+
+      if (!existingUser) {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            name: params.user.name ?? "",
+            image: params.user.image ?? "",
+            provider: "Google",
+          },
+        });
+      } else {
+       await prismaClient.user.update({
+          where: { email: params.user.email },
+          data: {
+            name: params.user.name ?? existingUser.name,
+            image: params.user.image ?? existingUser.image,
+          },
+        });
+      }
+    } catch (e) {
+      console.error("Error handling user sign-in:", e);
+      return false;
+    }
+
+      return true
+    }
+}
+})
+
+export {handler as GET, handler as POST}
